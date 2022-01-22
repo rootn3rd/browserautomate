0d6da7f028fd2bcb14423a6766019e6f46ae9538
Pankaj Chaurasia <krishnahare14@gmail.com>
2022-01-22 16:14:45 +0530

    Initial commit
    TODO - Cleanup and get active url

---
+++ AutomateBrowser/Controllers/BrowserAutomateController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutomateBrowser.Controllers
+{
+    static class Browsers
+    {
+        public const string Chrome = "chrome";
+        public const string Firefox = "firefox";
+
+        public static string ChromeLocation = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+        public static string FirefoxLocation = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+
+        public static string FireFoxDatabase = @"C:\Users\PANKAJ\AppData\Roaming\Mozilla\Firefox\Profiles\tc3timr4.default-release\places.sqlite";
+    }
+    [ApiController]
+    public class BrowserAutomate : ControllerBase
+    {
+        public BrowserAutomate()
+        {
+        }
+
+        [HttpGet]
+        [Route("start")]
+        public string Start(string browser, string url)
+        {
+            switch (browser)
+            {
+                case Browsers.Firefox:
+                    Process.Start(Browsers.FirefoxLocation, url);
+                    break;
+                case Browsers.Chrome:
+                    Process.Start(Browsers.ChromeLocation, url);
+                    break;
+                default:
+                    return "Invalid input";
+            }
+            return $"Success";
+
+        }
+
+        [HttpGet]
+        [Route("stop")]
+        public bool Stop(string broswer)
+        {
+            switch (broswer)
+            {
+                case Browsers.Firefox:
+                case Browsers.Chrome:
+                    var processes = Process.GetProcessesByName(broswer);
+                    processes.ToList().ForEach(p => p.Kill());
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+
+        [HttpGet]
+        [Route("cleanup")]
+        public async Task<bool> Cleanup(string browser)
+        {
+            switch (browser)
+            {
+                case Browsers.Firefox:
+                    var processes = Process.GetProcessesByName(browser);
+                    processes.ToList().ForEach(p => p.Kill());
+                    await Task.Delay(1000);
+
+                    Directory.Delete(@"C:\Users\PANKAJ\AppData\Roaming\Mozilla\Firefox\Profiles", true);
+
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+
+        [HttpGet]
+        [Route("geturl")]
+        public string GetUrl(string browser)
+        {
+            switch (browser)
+            {
+                case Browsers.Chrome:
+                   
+                    break;
+
+                case Browsers.Firefox:
+
+                    var visited = new List<Visit>();
+                    using (var connection = new SqliteConnection($"Data Source={Browsers.FireFoxDatabase}"))
+                    {
+                        connection.Open();
+
+                        var command = connection.CreateCommand();
+                        command.CommandText =
+                                            @"
+                                SELECT datetime(moz_historyvisits.visit_date/1000000,'unixepoch') AS visittime, moz_places.url AS url
+                                FROM moz_places, moz_historyvisits 
+                                WHERE moz_places.id = moz_historyvisits.place_id
+                                ORDER BY visittime DESC
+                                LIMIT 1
+                        ";
+
+                        using var reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            visited.Add(new Visit((Convert.ToDateTime(reader["visittime"])), reader["url"].ToString()));
+                        }
+                    }
+
+                    var lastVisited = visited.OrderByDescending(c => c.timeVisited).FirstOrDefault();
+                    return lastVisited != null ? lastVisited.url : "";
+                default:
+                    break;
+            }
+            return "";
+        }
+
+        record Visit (DateTime timeVisited, string url);
+    }
+}
---
+++ SettingsReader/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace SettingsReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var path = @"D:\delete\cc\data_1";
+
+            using var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using var reader = new BinaryReader(fileStream, Encoding.UTF8, false);
+            var ending = "\x00";
+
+            var endb = Encoding.UTF8.GetBytes(ending);
+            var str = new StringBuilder();
+            var bytes = reader.ReadBytes(4);
+            while (bytes.Length != 0)
+            {
+                if(bytes.SequenceEqual(endb))
+                {
+                    Console.WriteLine(str.ToString());
+                }
+
+                bytes = reader.ReadBytes(4);
+                str.Append(Encoding.UTF8.GetString(bytes));
+            }
+            var fullFile = reader.ReadString();
+
+            Console.WriteLine(reader.ReadString());
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
